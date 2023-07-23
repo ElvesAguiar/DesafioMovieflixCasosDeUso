21d2ed214516e9b4c3a819d8308febfae3086e36
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-23 10:03:45 -0300

    feat: inset Review endpoint

---
+++ src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/reviews")
+public class ReviewController {
+
+	@Autowired
+	private ReviewService service;
+	
+
+	@PostMapping
+	public ResponseEntity<ReviewDTO> insert(@RequestBody ReviewDTO dto) {
+		dto = service.insert(dto);
+		return ResponseEntity.ok(dto);
+	}
+}
