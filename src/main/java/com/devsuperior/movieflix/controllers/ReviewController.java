359bdabcf609d841276aad0ceb027df02f79aa15
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-23 10:13:09 -0300

    feat: refactor insert returning status created

--- src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/reviews")
@@ -20,6 +23,7 @@ public class ReviewController {
 	@PostMapping
 	public ResponseEntity<ReviewDTO> insert(@RequestBody ReviewDTO dto) {
 		dto = service.insert(dto);
-		return ResponseEntity.ok(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
